c362f75689f8ed08b16e4c530776861ac7ab6e40
Andi Ibrahim <ibrahimkdi12345@gmail.com>
2026-06-26 16:41:02 +0800

    profilkuhhh

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Inisialisasi AOS (Animate On Scroll)
+AOS.init();
+
+// Inisialisasi Typed.js untuk efek ketik berganti-ganti
+var typed = new Typed('.typing', {
+    strings: ['Computer Science Student', 'Web Developer', 'Java Programmer', 'Tech Enthusiast'],
+    typeSpeed: 70,
+    backSpeed: 50,
+    backDelay: 1500,
+    loop: true
+});
